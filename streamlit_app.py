038804f78555c5eac9d7ad86e3a6b96912c624df
muhammad-sharopov <82517976+muhammad-sharopov@users.noreply.github.com>
2025-02-17 19:40:31 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ with st.sidebar:
   st.header('Введите признак: ')
   island = st.selectbox('Island', ('Torgersen', 'Dream', 'Biscoe'))
   bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 44.5)
-  bill_degth_mm = st.slider('Bill degth (mm)', 13.1, 21.5, 17.3)
+  bill_depth_mm = st.slider('Bill degth (mm)', 13.1, 21.5, 17.3)
   flipper_length_mm = st.slider('Flipper length (mm)', 32.1, 59.6, 44.5)
   body_mass_g = st.slider('Body mass (g)', 32.1, 59.6, 44.5)
   gender = st.selectbox('Gender', ("female", 'male'))
