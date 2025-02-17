610a6caf2a76b5a6cf3f35fd625e1989d88391ac
muhammad-sharopov <82517976+muhammad-sharopov@users.noreply.github.com>
2025-02-17 19:02:00 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,3 +15,12 @@ with st.expander('Data'):
   st.write('y')
   y_raw = df.species
   st.dataframe(y_raw)
+
+with st.sidebar:
+  st.header('Введите признак: ')
+  island = st.selection('Island', ('Torgersen', 'Dream', 'Biscoe'))
+  bill_length_mm = st.slider('Bill length (mm)', 32.1, 59.6, 44.5)
+  bill_degth_mm = st.slider('Bill degth (mm)', 13.1, 21.5, 17.3)
+  flipper_length_mm = st.slider('Flipper length (mm)', 32.1, 59.6, 44.5)
+  body_mass_g = st.slider('Body mass (g)', 32.1, 59.6, 44.5)
+  gender = st.selection('Gender', ("female", 'male'))
