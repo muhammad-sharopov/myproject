c9bdaecc2a0d6d1b4a5d9d964b913013e6cb72b7
muhammad-sharopov <82517976+muhammad-sharopov@users.noreply.github.com>
2025-02-17 18:52:22 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,17 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 My Name Is No One')
 
 st.write('Hello no where!')
+
+df = pd.read_csv("https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv")
+
+with st.expander('Data'):
+  st.write("X")
+  X_raw = df.drop('species' , axis = 1)
+  st.dataframe(X_raw)
+  
+  st.write('y')
+  y_raw = df.species
+  st.dataframe(y_raw)
